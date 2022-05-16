a180e3bb7e4e7b7a2b2856d952e6a2a57d9ee4f2
Nicole Salceda <nsalceda@flipgive.com>
2022-05-16 18:11:46 -0400

    Wrapbook: debug auth attempt

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  apiKey: "AIzaSyALarAhcZ_3ocynPyFXFbAjtZZQmhuQEGA",
   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
